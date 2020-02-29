21f5d4857b0725966957dac2ac741e9689489b94
WahahaQ <vladkoval4yk399@gmail.com>
2020-02-29 22:14:24 +0200

    Initial commit

---
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Unispace", 71.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(285, 107);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "9999";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(285, 107);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Counter";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+	public partial class MainForm : Form
+	{
+		public string FilePath { get; private set; }
+
+		// Configuration:
+		private int Step = Convert.ToInt32(ConfigurationManager.AppSettings["Step"]);
+		private int MinNum = Convert.ToInt32(ConfigurationManager.AppSettings["MinNum"]);
+		private int MaxNum = Convert.ToInt32(ConfigurationManager.AppSettings["MaxNum"]);
+
+		public MainForm()
+		{
+			InitializeComponent();
+			FilePath = $"{Directory.GetCurrentDirectory()}\\CurrentPosition.txt";
+
+			if (!File.Exists(FilePath))
+			{
+				CreateFile();
+			}
+			else
+			{
+				int currentNumber = GetNumberFromFile();
+				SetNumberIntoFile(CheckCondition(currentNumber, MinNum, MaxNum) ? currentNumber : 0);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			RefreshMainLabel();
+		}
+
+		private void RefreshMainLabel()
+		{
+			label1.Text = GetNumberFromFile().ToString();
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			int currentNumber = GetNumberFromFile();
+
+			if (e.KeyCode == Keys.Add || e.KeyCode == Keys.Oemplus)
+			{
+				SetNumberIntoFile(CheckCondition(currentNumber + Step, MinNum, MaxNum) ? currentNumber + Step : currentNumber);
+				RefreshMainLabel();
+			}
+			else if (e.KeyCode == Keys.Subtract || e.KeyCode == Keys.OemMinus)
+			{
+				SetNumberIntoFile(CheckCondition(currentNumber - Step, MinNum, MaxNum) ? currentNumber - Step : currentNumber);
+				RefreshMainLabel();
+			}
+			else if (e.KeyCode == Keys.Delete)
+			{
+				SetNumberIntoFile(0);
+				RefreshMainLabel();
+			}
+		}
+
+		private void CreateFile()
+		{
+			if (!File.Exists(FilePath))
+			{
+				using (FileStream fs = File.Create(FilePath))
+				{
+					Byte[] info = new UTF8Encoding(true).GetBytes("0");
+					fs.Write(info, 0, info.Length);
+				}
+			}
+		}
+
+		private int GetNumberFromFile()
+		{
+			using (TextReader reader = File.OpenText(FilePath))
+			{
+				try
+				{
+					return Convert.ToInt32(reader.ReadLine());
+				}
+				catch
+				{
+					reader.Close();
+					File.Delete(FilePath);
+					CreateFile();
+					return GetNumberFromFile();
+				}
+			}
+		}
+
+		private void SetNumberIntoFile(int number)
+		{
+			using (StreamWriter sw = new StreamWriter(FilePath))
+			{
+				try
+				{
+					sw.WriteLine(number);
+				}
+				catch
+				{
+					sw.Close();
+					File.Delete(FilePath);
+					CreateFile();
+				}
+			}
+		}
+
+		Func<int, int, int, bool> CheckCondition = delegate (int number, int greaterThan, int lessThan)
+		{
+			return number >= greaterThan && number <= lessThan;
+		};
+	}
+}
